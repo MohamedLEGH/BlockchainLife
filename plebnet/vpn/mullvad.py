4f6c5a51887564f0b2ee9574d43431ef6bce4e2d
mr-spd <spd.bisesser@gmail.com>
2017-12-18 23:05:42 +0100

    Issue #4: Payment implemented and tested with testnet.

--- plebnet/vpn/mullvad.py
+++ plebnet/vpn/mullvad.py
@@ -1,5 +1,6 @@
 from robobrowser import RoboBrowser
-from cloudomate.wallet import Wallet
+#from cloudomate.wallet import Wallet
+import os
 
 class MullVad:
 	accountnumber = "6798499523758101"
@@ -33,16 +34,34 @@ class MullVad:
 				bitcoin_address = bitcoin_address_line.partition('"')[-1].rpartition('"')[0]
 		print(month_price)
 		print(bitcoin_address)
-		#return pay(bitcoin_address)
+		#if pay(month_price, bitcoin_address):
+		#	setupVPN()
+		#else:
+		#	print("Error: payment failed")
 
 	#Pay for 1 month using bitcoins and the electrum wallet
-	'''def pay(self, price, bitcoin_address):
-		transaction_hash = self.wallet.pay(bitcoin_address, price)
-		print(transaction_hash)
-		if transaction_hash:
-			return true
+	def pay(self, price, bitcoin_address):
+		#Start electrum daemon
+		os.system('electrum --testnet daemon start')
+		#Load electrum default wallet
+		os.system('electrum --testnet daemon load_wallet')
+		#Check balance in wallet is enough for payment
+		balance = os.popen('electrum --testnet getbalance').read()
+		balance = float(balance.split("\n")[1].split(":")[1].replace('"', "").replace(" ", "").replace(",", ""))
+		print(balance)
+		if balance >= price:
+			transaction = os.popen('electrum --testnet payto ' + bitcoin_address + ' ' + str(price) + '| electrum --testnet  broadcast -').read()
+			#Check if transaction was successfull and return state of transaction
+			transaction_complete = transaction.find('true')
+			if transaction_complete == -1:
+				transaction_complete = False
+			else:
+				transaction_complete = True
+			print('transaction = ' + str(transaction_complete))
+			return transaction_complete
 		else:
-			return false'''
+			print('Insufficient balance, transaction cancelled')
+			return False
 
 	#Setup the VPN
 	def setupVPN():
@@ -53,10 +72,15 @@ class MullVad:
 
 if __name__ == '__main__':
 	mv = MullVad()
-	mv.login()
-	#TODO: Add check if VPN is still valid before purchase
-	#TODO: Add check if VPN is already installed
-	if mv.purchase():
-		mv.setupVPN()
-	else:
-		print("Transaction failed")
+	test_scraping = False
+	test_payment = True
+	if test_scraping:
+		mv.login()
+		#TODO: Add check if VPN is still valid before purchase
+		#TODO: Add check if VPN is already installed
+		if mv.purchase():
+			mv.setupVPN()
+		else:
+			print("Transaction failed")
+	if test_payment:
+		mv.pay(0.00027, "mfXuna4dtqrKW827BpWjzqkKDyqUjuzPz5")
